517a90e50e3bc387ba9fbe7bf1cebd460f83370e
Mark Cassidy <mark@cassidy.dk>
2016-10-03 12:21:44 +0200

    Implemented IOC container configuration via IServicesConfigurator. Might need to re-think how the dependency on Microsoft.Extensions.DependencyInjection is laid out later.

---
+++ NitroNet.Sitecore/Configurator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NitroNet.Sitecore.Caching;
+using NitroNet.Sitecore.Rendering;
+using NitroNet.ViewEngine.TemplateHandler;
+using NitroNet.ViewEngine.TemplateHandler.Grid;
+using Sitecore;
+using Sitecore.DependencyInjection;
+using Sitecore.Mvc.Common;
+using Veil;
+
+namespace NitroNet.Sitecore
+{
+	public class Configurator : IServicesConfigurator
+	{
+		public void Configure(IServiceCollection serviceCollection)
+		{
+			serviceCollection.AddTransient(provider => GridContext.GetFromRenderingContext(ContextService.Get().GetCurrent<RenderingContext>()));
+			serviceCollection.AddTransient<ISitecoreRenderingRepository, SitecoreRenderingRepository>();
+			serviceCollection.AddTransient<ISitecoreCacheManager, SitecoreCacheManager>();
+			serviceCollection.AddTransient<INitroTemplateHandlerFactory, SitecoreMvcNitroTemplateHandlerFactory>();
+			serviceCollection.AddTransient(provider => Context.Database);
+		}
+	}
+}

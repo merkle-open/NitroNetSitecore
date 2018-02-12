9abac8bbbb5fd2d0fbda6de1177ec3559db41a29
Amir Begic <amir.begic@namics.com>
2018-02-12 15:28:01 +0100

    Added the pipeline and class for registering the view engine. Saving an additional step in the installation.

---
+++ NitroNet.Sitecore/Registration/RegisterNitroNetViewEngine.cs
@@ -0,0 +1,13 @@
+﻿using Sitecore.Pipelines;
+using System.Web.Mvc;
+
+namespace NitroNet.Sitecore.Registration
+{
+    public class RegisterNitroNetViewEngine
+    {
+        public virtual void Process(PipelineArgs args)
+        {
+            ViewEngines.Engines.Add(DependencyResolver.Current.GetService<SitecoreNitroNetViewEngine>());
+        }
+    }
+}
